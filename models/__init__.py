a9f0902aec84d02a873735b2652bb5a8a07cf7e9
Pierre Dramaix <ebmtech.org@gmail.com>
2025-08-01 09:30:19 +0200

    [order] forgot to update init file

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from . import ord_attachment_leg
+from . import ord_main
+from . import ord_ticket
+from . import ord_attachment
+from . import ord_supplier
+from . import ord_destination
+from . import ord_supplier_status
